ad7f3ffc560e2e95403bfa61ad62efd62fad87fe
UN-9BOT <un.9bot@gmail.com>
2023-04-04 09:44:22 +0300

    complete ex5

--- St_Prata_C/ch14/ex5.c
+++ St_Prata_C/ch14/ex5.c
@@ -1,26 +1,46 @@
 #include "ex5.h"
-#include <stdlib.h>
-
 
 int main(void) {
     t_student *arr = malloc(sizeof(t_student) * CSIZE);
-    
-    for (int i = 0; i < CSIZE; i++) {
-        arr[i].bio.nm = malloc(sizeof(char) * 10);
-        printf("input %d - name\n", i);
-        scanf("%s", arr[i].bio.nm);
-    }
-
-    for (int i = 0; i < CSIZE; i++) {
-        printf("out %s - name\n", arr[i].bio.nm);
-    }
-
-
-    for (int i = 0; i < CSIZE; i++) { 
-        free(arr[i].bio.nm);
-    }
-    free(arr);
+    inData(arr);
+    printData(arr);
+    freeAlloc(arr);
 
     return (0);
 }
 
+void inData(t_student *arr) {
+    for (int i = 0; i < CSIZE; i++) {
+        arr[i].bio.nm = malloc(sizeof(char) * 10);
+        arr[i].bio.sn = malloc(sizeof(char) * 10);
+        for (int j = 0; j < rand() % 10 + 3; j++) {
+            if (j == 0 ) {
+                arr[i].bio.nm[j] = 'A' + (rand() % 26);
+                arr[i].bio.sn[j] = 'A' + (rand() % 26);
+            } else {
+                arr[i].bio.nm[j] = 'a' + (rand() % 26);
+                arr[i].bio.sn[j] = 'a' + (rand() % 26); 
+            }
+        }
+        int sum = 0;
+        for (int j = 0; j < 3; j++) {
+            arr[i].grade[j] = rand() % 10;
+            sum += arr[i].grade[j];
+        }
+        arr[i].mid_grade = ((float)sum / 3);
+    }
+}
+
+void freeAlloc(t_student *arr) {
+    for (int i = 0; i < CSIZE; i++) { 
+        free(arr[i].bio.nm);
+        free(arr[i].bio.sn);
+    }
+    free(arr);
+}
+
+void printData(t_student *arr) {
+    for (int i = 0; i < CSIZE; i++) {
+        printf("%d. %s %s %.1f\n", i, arr[i].bio.nm, arr[i].bio.sn, arr[i].mid_grade);
+    }
+}
--- St_Prata_C/ch14/ex5.h
+++ St_Prata_C/ch14/ex5.h
@@ -1,14 +1,25 @@
+#ifndef EX5
+#define EX5
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+#include <math.h>
+
 #define CSIZE 4
 
 typedef struct Name {
     char *nm;
     char *sn;
 } t_name;
+
 typedef struct Student {
     t_name bio;
     float grade[3];
     float mid_grade;
 } t_student;
+
+void inData(t_student *arr);
+void freeAlloc(t_student *arr);
+void printData(t_student *arr);
+#endif
